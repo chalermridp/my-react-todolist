f8347cba92605b9f68358fac5d60fcbd41dc5203
Chalermrid Patipanang <chalermrid.patipanang@tesco.com>
2020-12-14 13:20:46 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,115 @@
-import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import React, { useEffect, useState } from 'react'
+import { Container } from 'react-bootstrap';
+import axios from 'axios';
+
+
+function InsertBox({ saveTodo }) {
+  const [text, setText] = useState("")
+  return (
+    <div className="input-group">
+      <input
+        type="text"
+        className="form-control"
+        onChange={(e) => {
+          setText(e.target.value)
+        }}
+      >
+      </input>
+      <button
+        className="btn btn-primary"
+        onClick={() => {
+
+          saveTodo(text)
+
+        }}
+      >
+        Add
+      </button>
+    </div>
+  )
+}
+
+function ToDoList({ todoList, deleteTodo }) {
+  return (
+    <div>
+      <hr />
+      {
+        todoList.map((value) => {
+          return (
+            <ToDoItem content={value} deleteTodo={deleteTodo} key={value.id} />
+          )
+        })
+      }
+    </div>
+  )
+}
+
+function ToDoItem({ content, deleteTodo }) {
+  return (
+    <div className="form-group">
+      <span>{content.text}</span>
+      <button className="btn btn-sm btn-danger" onClick={() => {
+        deleteTodo(content.id)
+      }}>x</button>
+    </div>
+  )
+}
 
 function App() {
+
+  const [todoList, setTodoList] = useState([])
+
+  //
+  useEffect(() => {
+
+    async function fetchData() {
+      const todo = await getTodoList()
+      setTodoList(todo)
+    }
+
+    fetchData()
+    //  .then(res => {
+    //    setTodoList(res)
+    //  })
+    // console.log("test")
+    // console.log(test)
+
+  }, [])
+
+  const deleteTodo = async (id) => {
+    try {
+      await axios.delete(`http://localhost:3001/todo-list/${id}`)
+      setTodoList(todoList.filter(i => i.id !== id))
+    }
+    catch (e) {
+      alert("cannot delete")
+    }
+  }
+
+  const saveTodo = async (text) => {
+    const newData = { id: Date.now(), text: text, is_done: false }
+    try {
+      await axios.post('http://localhost:3001/todo-list', newData)
+      setTodoList([...todoList, newData])
+    }
+    catch (e) {
+      alert("cannot save")
+    }
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <InsertBox todoList={todoList} saveTodo={saveTodo} />
+      <ToDoList todoList={todoList} deleteTodo={deleteTodo} />
+    </Container>
   );
 }
+
+async function getTodoList() {
+  const response = await axios.get('http://localhost:3001/todo-list')
+  const result = response.data;
+  return result;
+};
 
 export default App;
